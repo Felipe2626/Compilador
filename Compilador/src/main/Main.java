bf94228f97fc97a4a4e4a4ee04c0823997951c83
Gabriel Melo <gabrielsilvamelo96@gmail.com>
2019-05-13 23:28:41 -0300

    args

--- Compilador/src/main/Main.java
+++ Compilador/src/main/Main.java
@@ -10,7 +10,7 @@ import symbols.Env;
 public class Main {
 
 	public static void main(String[] args ) {// throws IOException
-		String filepath = new String ("src/Test/test8.txt");
+		String filepath = new String (args[0]);
 		FileReader fr = null;
 		ArrayList<Token> lToken = new ArrayList<Token>();
 		try {
